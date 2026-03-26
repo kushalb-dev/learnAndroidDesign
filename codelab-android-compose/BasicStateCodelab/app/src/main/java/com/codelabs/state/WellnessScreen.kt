97ee4a35beed43bd83cff477ad28df1b4e5733ba
kushalb-dev <kushalb.dev@gmail.com>
2026-03-26 17:35:51 +0545

    Go home now boiii!

--- codelab-android-compose/BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
+++ codelab-android-compose/BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -15,14 +15,13 @@
  */
 package com.codelabs.state
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
 ) {
     StatefulCounter(modifier)
+    WellnessTasksList(modifier)
 }
